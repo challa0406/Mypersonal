0314a2b6a6750ecfef351200144e550cf25d919b
Srinivasulu Challa <srinivasulu.challa@autodesk.com>
2016-10-04 13:51:45 +0530

    New File Added

---
+++ Insert.sql
@@ -0,0 +1,55 @@
+insert into t_test values (
+999991460062444
+,'t_schalla'
+,'UCM Integration Program'
+,501	
+,'ActivitityName1'
+,'banglore'	
+,'test.user1'	
+,10	
+,'this recording for testing 1 '
+,'manager'	
+,'Service'	
+,'banglore'
+,'email'	
+,'adsk'
+,'United States'
+,'US'
+,'United States'
+,988889	
+,'marketing'
+,'N'	
+,'Banglore_1@test.com'
+,6587
+,'Challa_test1'
+,'MFG'
+,'MFG - Industrial Mach'
+,'Consumer Packaged Goods'
+,'testnotes'
+,'MFG'
+,'MFG'
+,'manager'
+,'Srini_Test1'
+,'MFGmanager@test.com'
+,'testlead'
+,'testlist'
+,'UCM Sync Leads List'
+,'United States'
+,810000	
+,'hoffmans'
+,546354	
+,4738922
+,'ENU'
+,'Product Manager'
+,'testprogram'
+,'testqualification'
+,'testresponse'
+,'MFG'
+,'MFG'
+,'Connecticut'
+,'testterritory'
+,'testunsubscribed'
+,'testwebsite.com'
+,'2016-04-26 10:59:24'
+);
+
